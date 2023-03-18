283b9c0d4332f3e8cb149e84a35118a028194dcd
Hyuk Kwon <poiu03290@naver.com>
2023-03-18 14:07:27 +0900

    google ga 수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import ReactGA from 'react-ga';
-
 import { LayOut } from './components/LayOut';
 import { Main } from "./view/Main";
 import { PostDetail } from './view/PostDetail';
@@ -9,17 +7,13 @@ import { About } from './view/About';
 
 import './App.css';
 
-const TRACKING_ID = process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID;
-ReactGA.initialize(TRACKING_ID, { debug: true });
-ReactGA.pageview(window.location.pathname);
-
 function App() {
   return (
     <div className='container'>
       <BrowserRouter>
         <Routes>
           <Route element={<LayOut />}>
-            <Route path={`${process.env.PUBLIC_URL}/`} element={<Main />}></Route>
+            <Route path='/' element={<Main />}></Route>
             <Route path='/post/:postId' element={<PostDetail />}></Route>
             <Route path='/post' element={<Post />}></Route>
             <Route path='/about' element={<About />}></Route>
